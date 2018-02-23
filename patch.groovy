c03fb8acd1041b6abc2b6f7d08b6bf19703cb4d2
roshnew <35692113+roshnew@users.noreply.github.com>
2018-02-22 22:51:21 -0600

    patch

---
+++ patch.groovy
@@ -0,0 +1,42 @@
+#!groovy
+node {
+
+    stage("starting") {
+         	    
+    }
+    
+     stage("Patch Servers with Chef Knife") {
+          try {
+
+            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "${credentialsId}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+                    sh "knife ssh chef_environment:${chef_env} 'sudo -S ${command}' -P '${PASSWORD}' -x ${USERNAME}"
+              }
+          } catch (e) {
+              currentBuild.result = "FAILED"
+              notifyFailed("${failure_email_address}")
+              throw e
+          }
+
+     }
+     
+
+     stage("ending") {
+         
+     }
+
+}
+
+
+
+def notifyFailed(failure_email_address) {
+		def details = """Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':
+		Check console output at '${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]"""
+		
+	  emailext (
+		  to: '${failure_email_address}',
+		  from: 'azu-jenkins02@footlocker.com',
+		  subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+		  body: details,
+		  recipientProviders: [[$class: 'DevelopersRecipientProvider']]
+		)
+}
