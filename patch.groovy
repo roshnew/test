ca7e772e2bde7a4dd195fa7c617a018a9ca52905
roshnew <35692113+roshnew@users.noreply.github.com>
2018-03-21 10:22:44 -0500

    changed credentialsid

--- patch.groovy
+++ patch.groovy
@@ -8,7 +8,7 @@ node {
      stage("Patch Servers with Chef Knife") {
           try {
 
-            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "${credentialsId}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "${credentials}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
                     sh "knife ssh tags:${chef_env} 'sudo -S ${command}' -P '${PASSWORD}' -x ${USERNAME}"
               }
           } catch (e) {
