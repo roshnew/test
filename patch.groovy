547974a478bdc715b18fb30f97badea2c27ca7ef
roshnew <35692113+roshnew@users.noreply.github.com>
2018-03-22 03:43:03 -0500

    removed email

--- patch.groovy
+++ patch.groovy
@@ -11,11 +11,7 @@ node {
             withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "${credentials}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
                     sh "knife ssh tags:${chef_env} 'sudo -S ${command}' -P '${PASSWORD}' -x ${USERNAME}"
               }
-          } catch (e) {
-              currentBuild.result = "FAILED"
-              notifyFailed("${failure_email_address}")
-              throw e
-          }
+          } 
 
      }
      
