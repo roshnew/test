97e33e32ad37a345b0d2a4188beca25f62ca3d7b
roshnew <35692113+roshnew@users.noreply.github.com>
2018-04-03 03:51:31 -0500

    Update patch.groovy

--- patch.groovy
+++ patch.groovy
@@ -8,9 +8,9 @@ node {
      stage("Patch Servers with Chef Knife") {
           try {
 
-            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "${credentials}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-                    sh "knife ssh tags:${chef_env} 'sudo -S ${command}' -P '${PASSWORD}' -x ${USERNAME}"
-                      }
+            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "${credentialsId}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+                    sh "knife ssh name:${hostname} 'sudo -S ${command}' -P '${PASSWORD}' -x ${USERNAME}"
+              }
           } catch (e) {
               currentBuild.result = "FAILED"
               notifyFailed("${failure_email_address}")
@@ -28,6 +28,15 @@ node {
 
 
 
-
-
-
+def notifyFailed(failure_email_address) {
+		def details = """Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':
+		Check console output at '${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]"""
+		
+	  emailext (
+		  to: '${failure_email_address}',
+		  from: 'azu-jenkins02@footlocker.com',
+		  subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+		  body: details,
+		  recipientProviders: [[$class: 'DevelopersRecipientProvider']]
+		)
+}
